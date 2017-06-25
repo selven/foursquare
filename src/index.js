ea33b570cfcc7f7b9cf0ab052d2bfbad7358efb3
selven <rossjj@gmail.com>
2017-06-25 14:11:19 +0100

    folder structure, add handlebars, jquery

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import search from './search';
+
+const template = require('./templates/results.handlebars');
+
+search.init();
---
+++ src/search.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+
+export default {
+	events : function() {
+		
+	},
+	
+	init : function() {
+		console.log('hello');
+	}
+}
